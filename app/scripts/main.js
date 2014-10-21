86b88b99d60c50b5ef08d984d7ee955a178901de
Tyler Vincent Renfro <v.renfro6@gmail.com>
2014-10-21 17:20:40 -0500

    added scroll to

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!');
+
+$(".learn-more").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(".marketing").offset().top
+    }, 400);
+  });
